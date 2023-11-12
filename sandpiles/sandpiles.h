0f1aed7f063eccae13644d5ba14b1f7e14395b2e
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-11-12 16:16:59 +0100

    header file

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,13 @@
+#define SANDPILES_H
+#ifndef SANDPILES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid(int grid[3][3]);
+void sum_grids(int grid1[3][3], int grid2[3][3]);
+void change_grids(int grid1[3][3]);
+int check_grids(int grid1[3][3]);
+
+#endif
