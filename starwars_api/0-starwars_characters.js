e32872bd56769f965a22748cf8935e7c4cbeba12
Ali-Dehissy <ali1998dehissy@gmail.com>
2024-10-05 12:58:09 +0200

    api fetch

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+const request = require('request');
+
+function fetchCharacter (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) return reject(error);
+      resolve(JSON.parse(body).name);
+    });
+  });
+}
+
+async function getCharacterNames (movieId) {
+  const movieUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+  request(movieUrl, async (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const movieData = JSON.parse(body);
+    const characterUrls = movieData.characters;
+
+    for (const url of characterUrls) {
+      try {
+        const name = await fetchCharacter(url);
+        console.log(name);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  });
+}
+
+const movieId = process.argv[2];
+
+getCharacterNames(movieId);
