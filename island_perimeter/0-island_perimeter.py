7057e98d90f41aef6cd16431ebc1e6a1a8afcbf5
Ali-Dehissy <ali1998dehissy@gmail.com>
2024-10-12 16:53:22 +0200

    update

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Perimetre
+perimeter of the island described in grid
 """
 
 
 def island_perimeter(grid):
     """
-    Grid on ints
+    grid is a list of list of integers
     """
     if not grid or not grid[0]:
         return 0
@@ -15,13 +15,13 @@ def island_perimeter(grid):
     perimeter = 0
 
     for i in range(rows):
-        for x in range(cols):
-            if grid[i][x] == 1:
+        for j in range(cols):
+            if grid[i][j] == 1:
                 perimeter += 4
 
-                if i > 0 and grid[i - 1][x] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                if x > 0 and grid[i][x - 1] == 1:
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
 
     return perimeter
