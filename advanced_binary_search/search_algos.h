d894756d4a2f0041fccbb8c008d9a09ce9228826
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2024-10-23 21:56:44 +0100

    update

--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -1,9 +1,11 @@
-#define SEARCH_ALGOS_H
-#ifndef SEARCH_ALGOS_H
-
+#ifndef SEARCH_H
+#define SEARCH_H
 #include <stdlib.h>
 #include <stdio.h>
 
+void print_array(int *array, int first, int last);
+int recursive_search(int *array, size_t first, size_t last, int value);
+int linear_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
 #endif
